45a1d97e98eb3321f0522d45443bc0791d48628c
kimchan-kimchan <63887556+kimchan-kimchan@users.noreply.github.com>
2020-04-25 13:20:56 +0900

    Add files via upload

---
+++ 0425/turtle01.py
@@ -0,0 +1,12 @@
+import turtle as t
+t.shape("turtle")
+t.write(t.position())
+t.forward(100)
+t.write(t.position())
+t.left(120)
+t.forward(100)
+t.write(t.position())
+t.left(120)
+t.forward(100)
+t.left(120)
+
